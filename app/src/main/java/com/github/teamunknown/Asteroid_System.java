560ae6265a6337a5e0afb78f55ec27d11600171a
Alex Hornsby <alexahornsby@hotmail.co.uk>
2020-03-21 23:08:32 +0000

    Add in the movemnt of background to the github project and more!
    - Add background movement into project
    
    - Add new class to host all background movement
    
    - Add spawning of astroids (Alpha)
    
    - Add Images for astroids into screen  (Alpha)
    
    - Add Movemnet of astroids  (Alpha)
    
    - Lastly slight clean up of code

--- app/src/main/java/com/github/teamunknown/Asteroid_System.java
+++ app/src/main/java/com/github/teamunknown/Asteroid_System.java
@@ -1,21 +1,52 @@
 package com.github.teamunknown;
 
+import android.app.Activity;
+import android.widget.ImageView;
 import java.util.List;
+import java.util.Random;
 
 // This will be the class that runs the asteroid system. eg spawning, increaseing speed/amount and so on...
 public class Asteroid_System {
 
+    public Activity activity;
     // TODO: 02/03/2020 This is just a basic get up. Need to first look into how the system will work
-    // This class does nothing right now
-    private  int bondsLeft, bondsRight, bondsTop, bondsBottom;
+    // TODO: 21/03/2020 have add muiltpue arrays to store images, ImageView and if they are being used. need to do some research into Java type of struct/Dictionary
+
+    private int bondsLeft, bondsRight, bondsTop, bondsBottom;
     private int posY, posX;
     private int rotation;
 
+    private int randomNumImageView;
+    private int randomNumImage;
+
+    private ImageView imageviews[] = new ImageView[4];
+    private int img[] = new int[4];
+    private boolean isUsed[] = new boolean[4];
+
     private List<Asteroid> asteroids;
 
-    public void spawn(int size){
+    private Random rand = new Random(System.currentTimeMillis());
+
+    public Asteroid_System(Activity activity) {
+        this.activity = activity;
+        imageviews[0] = (ImageView) this.activity.findViewById(R.id.as1);
+        imageviews[1] = (ImageView) this.activity.findViewById(R.id.as2);
+        imageviews[2] = (ImageView) this.activity.findViewById(R.id.as3);
+        imageviews[3] = (ImageView) this.activity.findViewById(R.id.as4);
+
+        img[0] = R.drawable.asteroid2;
+        img[1] = R.drawable.asteroid2;
+        img[2] = R.drawable.asteroid3;
+        img[3] = R.drawable.asteroid3;
+
+        imageviews[0].setImageResource(R.drawable.asteroid3);
+        imageviews[0].setX(200);
+
+    }
+
+    public void spawn(int size) {
         //validating size given
-        if(size >= 1 && size <= 3) {
+        if (size >= 1 && size <= 3) {
 
         } else {
 
@@ -24,13 +55,35 @@ public class Asteroid_System {
         // astroids.add(new Astroid());
     }
 
-    public void destroyAsteroid(int asteroid){
+    public void destroyAsteroid(int asteroid) {
         //validating astroid given
-        if(asteroid >= 1 && asteroid <= asteroids.size()){
+        if (asteroid >= 1 && asteroid <= asteroids.size()) {
 
         } else {
 
         }
+
+    }
+
+    public void spawn() {
+        //imageviews[0].setY((rand.nextInt(2000)));
+        randomNumImageView = rand.nextInt(4);
+        while (isUsed[randomNumImageView]) {
+            randomNumImageView = rand.nextInt(4);
+        }
+        //isUsed[randomNumImageView] = true;
+        move();
+        randomNumImage = rand.nextInt(4);
+        imageviews[randomNumImage].setImageResource(img[randomNumImage]);
+
+    }
+
+    public void move() {
+        imageviews[randomNumImageView].setX(rand.nextInt(2000) - 1000);
+        imageviews[randomNumImageView].setY(rand.nextInt(2000) - 1000);
+    }
+
+    public void destroy() {
 
     }
 }
